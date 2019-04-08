5e5df56cf6a2aaf601eae01e9596713954f84101
dlaqkqh1 <dlacksdn12@naver.com>
2019-04-08 16:15:45 +0900

    0408

--- be/routes/api/index.js
+++ be/routes/api/index.js
@@ -1,20 +1,18 @@
-var createError = require('http-errors')
 var express = require('express');
 var router = express.Router();
-
-router.get('/users', function(req, res, next) {
-  res.send({ a : 1 })
-});
-
-router.post('/users', function(req, res, next) {
-  res.send({ a : 1 })
-});
-
-router.use('/test', require('./test'));
-router.use('/user', require('./user'));
-
+var createError = require('http-errors');
 
 /* GET home page. */
+router.get('/test', function(req, res, next) {
+  res.send({ msg : 'hello', a: 1 });
+});
+
+router.get('/best', function(req, res, next) {
+  res.send({ msg : 'best', a : 2 });
+});
+
+router.use('/user', require('./user'));
+
 router.all('*', function(req, res, next) {
   next(createError(404,'API를 찾을 수 없습니다.'));
 });
--- fe/src/App.vue
+++ fe/src/App.vue
@@ -14,6 +14,7 @@
           value="true"
           v-for="(item, i) in items"
           :key="i"
+          :to="item.to"
         >
           <v-list-tile-action>
             <v-icon v-html="item.icon"></v-icon>
@@ -79,9 +80,20 @@ export default {
       drawer: true,
       fixed: false,
       items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
+        icon: 'home',
+        title: '홈',
+        to:{
+          path:'/'
+        }
+      },
+      {
+        icon:'face',
+        title:'사용자',
+        to:{
+          path:'/about'
+        }
+      }
+      ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
